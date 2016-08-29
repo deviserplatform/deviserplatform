bcd8c837ed7b9c9e1b937b276bd0f11216092e1b
Karthick Sundararajan <sky.karthick@gmail.com>
2016-08-29 23:53:21 +0200

    1) Test Project Setup 2) Targetting netcore and .net framework with latest tools

---
+++ test/Deviser.Core.Data.Test/DataProviders/ContentTypeProviderTest.cs
@@ -0,0 +1,107 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Deviser.Core.Data.Entities;
+using Deviser.WI;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features.Authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Deviser.Core.Data.DataProviders
+{
+    public class ContentTypeProviderTest
+    {
+        private readonly ILifetimeScope container;
+        private readonly IServiceProvider serviceProvider;
+
+        public ContentTypeProviderTest()
+        {
+            var efServiceProvider = new ServiceCollection().AddEntityFrameworkInMemoryDatabase().BuildServiceProvider();
+
+            var services = new ServiceCollection();
+            services.AddOptions();
+            services
+                .AddDbContext<DeviserDBContext>(b => b.UseInMemoryDatabase().UseInternalServiceProvider(efServiceProvider));
+
+            //services.AddIdentity<User, Role>()
+            //        .AddEntityFrameworkStores<DeviserDBContext>();
+
+            services.AddLogging();
+            services.AddOptions();
+
+            // IHttpContextAccessor is required for SignInManager, and UserManager
+            //var context = new DefaultHttpContext();
+            //context.Features.Set<IHttpAuthenticationFeature>(new HttpAuthenticationFeature() { Handler = new TestAuthHandler() });
+            //services.AddSingleton<IHttpContextAccessor>(
+            //    new HttpContextAccessor()
+            //    {
+            //        HttpContext = context,
+            //    });
+
+            // Add Autofac
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterModule<DefaultModule>();
+            containerBuilder.Populate(services);
+            container = containerBuilder.Build();
+            serviceProvider = new AutofacServiceProvider(container);
+        }
+
+        [Fact]
+        public void PassingTest()
+        {
+            //Arrange
+            var contentTypeProvider = new ContentTypeProvider(container);
+            var dbContext = serviceProvider.GetRequiredService<DeviserDBContext>();
+            var contentType = new ContentType
+            {
+                Name = "TypeName",
+                Label = "Type Name",
+            };
+
+            //Act
+            var result = contentTypeProvider.CreateContentType(contentType);
+
+            //Assert
+            Assert.NotNull(result);
+        }
+
+        private class TestAuthHandler : IAuthenticationHandler
+        {
+            public void Authenticate(AuthenticateContext context)
+            {
+                context.NotAuthenticated();
+            }
+
+            public Task AuthenticateAsync(AuthenticateContext context)
+            {
+                context.NotAuthenticated();
+                return Task.FromResult(0);
+            }
+
+            public Task ChallengeAsync(ChallengeContext context)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void GetDescriptions(DescribeSchemesContext context)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task SignInAsync(SignInContext context)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task SignOutAsync(SignOutContext context)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
