1b3fa7ef68a388ee7a4dbb3ce0eda43c16714ba5
Karthick Sundararajan <sky.karthick@gmail.com>
2020-09-08 22:32:35 +0200

    Export data functionality has been implemented in a controller

--- src/Deviser.Core/Deviser.Core.Data/Entities/Role.cs
+++ src/Deviser.Core/Deviser.Core.Data/Entities/Role.cs
@@ -16,7 +16,7 @@ namespace Deviser.Core.Data.Entities
             Name = roleName;
         }
 
-        public virtual ICollection<UserRole> UserRoles { get; } = new List<UserRole>();
+        public virtual ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
         public virtual ICollection<PagePermission> PagePermissions { get; set; }
         public virtual ICollection<ModulePermission> ModulePermissions { get; set; }
         public virtual ICollection<ContentPermission> ContentPermissions { get; set; }
