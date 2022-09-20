c82e7bc7fe0cdef78223733c4d6ef468aadd238d
Karthick Sundararajan <sky.karthick@gmail.com>
2022-09-20 19:55:12 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
  #!/bin/bash
-dotnet msbuild build.targets -property:Configuration=Release;VersionSuffix=${{github.run_number}}
+dotnet msbuild build.targets -property:Configuration=Release;VersionSuffix=$buildNumber
