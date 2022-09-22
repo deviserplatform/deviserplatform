6dd814530722e90fbb66413f95dff780c2bc29be
Karthick Sundararajan <sky.karthick@gmail.com>
2022-09-22 17:42:05 +0200

    Nuget Package Icon and Meta Info  (#61)
    * nuget package meta info update
    
    * nuget package icon has been added to all projects

--- build.ps1
+++ build.ps1
@@ -1,1 +1,4 @@
-dotnet msbuild build.targets -property:Configuration=Release
+
+$buildNumber=$args[0]
+write-host "build number: $buildNumber"
+dotnet msbuild build.targets /p:"Configuration=Release;VersionSuffix=beta2.$buildNumber"
