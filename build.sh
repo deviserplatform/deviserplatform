e70c6a89f1e4b9608ce9a78835a25a68350eaad9
Karthick Sundararajan <sky.karthick@gmail.com>
2022-10-15 16:14:19 +0200

    Develop (#70)
    * deviser-shared and deviser admin app have been upgraded to Angular 13
    
    * Deviser.Admin.Web has been successfully upgraded to Angular 13
    
    * Deviser.Web has been upgraded to Angualr 13
    
    * ngx-quill upgraded to Angular 13 compatible version
    
    * version update

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
  #!/bin/bash
 echo "build number: ${1}"
-dotnet msbuild build.targets /p:"Configuration=Release;VersionSuffix=beta2.${1}"
+dotnet msbuild build.targets /p:"Configuration=Release;VersionSuffix=beta3.${1}"
