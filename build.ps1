cd4f71bcaf16658c4d0ba89d2422f344f4218ce1
Karthick Sundararajan <sky.karthick@gmail.com>
2022-10-15 16:12:02 +0200

    version update

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 
 $buildNumber=$args[0]
 write-host "build number: $buildNumber"
-dotnet msbuild build.targets /p:"Configuration=Release;VersionSuffix=beta2.$buildNumber"
+dotnet msbuild build.targets /p:"Configuration=Release;VersionSuffix=beta3.$buildNumber"
